d1ae76891e31579d9a66328a630a985620f9b620
Steve Ryan <steve.ryan@asu.edu>
2021-03-31 13:33:09 -0700

    Full file replacement with the contents of UDS Divi.

--- header.php
+++ header.php
@@ -1,304 +1,205 @@
-<!DOCTYPE html>
-<!--[if IE 6]>
-<html id="ie6" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 7]>
-<html id="ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html id="ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<?php elegant_description(); ?>
-	<?php elegant_keywords(); ?>
-	<?php elegant_canonical(); ?>
-
-	<?php do_action( 'et_head_meta' ); ?>
-
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-	<?php $template_directory_uri = get_template_directory_uri(); ?>
-	<!--[if lt IE 9]>
-	<script src="<?php echo esc_url( $template_directory_uri . '/js/html5.js"' ); ?>" type="text/javascript"></script>
-	<![endif]-->
-
-	<script type="text/javascript">
-		document.documentElement.className = 'js';
-	</script>
-
-	<?php wp_head(); ?>
-	<!-- Begin ASU Heads -->
-	<?php asuwp_load_global_head_scripts(); ?>
-	<!-- END ASU Heads -->
-</head>
-<body <?php body_class(); ?>>
-<a href="#skippy" class="sr-only">Skip to Content</a>
-
-<!-- ******************* Google Tag Manager ASU Universal ******************* -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KDWN8Z"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KDWN8Z');</script>
-<!-- End Google Tag Manager ASU Universal -->
-
-<!-- Begin ASU Header  -->
-<?php asuwp_load_global_header(); ?>
-<!-- END ASU Header -->
-
 <?php
-	$product_tour_enabled = et_builder_is_product_tour_enabled();
-	$page_container_style = $product_tour_enabled ? ' style="padding-top: 0px;"' : ''; ?>
-	<div id="page-container"<?php echo $page_container_style; ?>>
-<?php
-	if ( $product_tour_enabled || is_page_template( 'page-template-blank.php' ) ) {
-		return;
-	}
+/**
+ * The header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package uds-wordpress-theme
+ */
 
-	$et_secondary_nav_items = et_divi_get_top_nav_items();
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
-	$et_phone_number = $et_secondary_nav_items->phone_number;
+$c_options              = array();
+$asu_hub_analytics     = 'disabled';
+$site_gtm_container_id = '';
+$site_ga_tracking_id   = '';
+$hotjar_site_id        = '';
+$nav_menu_enabled      = '';
 
-	$et_email = $et_secondary_nav_items->email;
+// retrieve settings from the theme mods entry in the options database table.
+$c_options = get_theme_mods();
 
-	$et_contact_info_defined = $et_secondary_nav_items->contact_info_defined;
+// Is navigation menu enabled?
+if ( ! empty( $c_options['header_navigation_menu'] ) ) {
+	$nav_menu_enabled = $c_options['header_navigation_menu'];
+}
 
-	$show_header_social_icons = $et_secondary_nav_items->show_header_social_icons;
-
-	$et_secondary_nav = $et_secondary_nav_items->secondary_nav;
-
-	$et_top_info_defined = $et_secondary_nav_items->top_info_defined;
-
-	$et_slide_header = 'slide' === et_get_option( 'header_style', 'left' ) || 'fullscreen' === et_get_option( 'header_style', 'left' ) ? true : false;
+// Do we have an asu_hub_analytics setting?
+if ( ! empty( $c_options['asu_hub_analytics'] ) ) {
+	$asu_hub_analytics = $c_options['asu_hub_analytics'];
+}
+// Do we have a site_gtm_container_id setting?
+if ( ! empty( $c_options['site_gtm_container_id'] ) ) {
+	$site_gtm_container_id = $c_options['site_gtm_container_id'];
+}
+// Do we have a site_ga_tracking_id setting?
+if ( ! empty( $c_options['site_ga_tracking_id'] ) ) {
+	$site_ga_tracking_id = $c_options['site_ga_tracking_id'];
+}
+// Do we have an hotjar_site_id setting?
+if ( ! empty( $c_options['hotjar_site_id'] ) ) {
+	$hotjar_site_id = $c_options['hotjar_site_id'];
+}
 ?>
 
-	<?php if ( $et_top_info_defined && ! $et_slide_header || is_customize_preview() ) : ?>
-		<div id="top-header"<?php echo $et_top_info_defined ? '' : 'style="display: none;"'; ?>>
-			<div class="container clearfix">
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 
-			<?php if ( $et_contact_info_defined ) : ?>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
 
-				<div id="et-info">
-				<?php if ( '' !== ( $et_phone_number = et_get_option( 'phone_number' ) ) ) : ?>
-					<span id="et-info-phone"><?php echo et_sanitize_html_input_text( $et_phone_number ); ?></span>
-				<?php endif; ?>
+	<?php
+	if ( is_user_logged_in() && ! is_customize_preview() ) {
+		// shift page content below the WP Admin toolbar.
+		?>
+		<style type="text/css" media="screen">
+			#asu-header.fixed-top {
+				top: 32px !important;
+			}
 
-				<?php if ( '' !== ( $et_email = et_get_option( 'header_email' ) ) ) : ?>
-					<a href="<?php echo esc_attr( 'mailto:' . $et_email ); ?>"><span id="et-info-email"><?php echo esc_html( $et_email ); ?></span></a>
-				<?php endif; ?>
+			#wpadminbar {
+				z-index: 999999 !important;
+			}
+		</style>
+		<?php
+	}
 
-				<?php
-				if ( true === $show_header_social_icons ) {
-					get_template_part( 'includes/social_icons', 'header' );
-				} ?>
-				</div> <!-- #et-info -->
+	// ASU Hub Analytics.
+	if ( ! empty( $asu_hub_analytics ) && 'enabled' === $asu_hub_analytics ) {
+		include get_stylesheet_directory() . '/inc/analytics/asu-hub-analytics-tracking-code.php';
+	}
 
-			<?php endif; // true === $et_contact_info_defined ?>
+	// Site Google Tag Manager.
+	if ( ! empty( $site_gtm_container_id ) ) {
+		include get_stylesheet_directory() . '/inc/analytics/google-tag-manager-tracking-code.php';
+	}
 
-				<div id="et-secondary-menu">
-				<?php
-					if ( ! $et_contact_info_defined && true === $show_header_social_icons ) {
-						get_template_part( 'includes/social_icons', 'header' );
-					} else if ( $et_contact_info_defined && true === $show_header_social_icons ) {
-						ob_start();
+	// Site Google Analytics.
+	if ( ! empty( $site_ga_tracking_id ) ) {
+		include get_stylesheet_directory() . '/inc/analytics/google-analytics-tracking-code.php';
+	}
 
-						get_template_part( 'includes/social_icons', 'header' );
+	// Hotjar Analytics.
+	if ( ! empty( $hotjar_site_id ) ) {
+		include get_stylesheet_directory() . '/inc/analytics/hotjar-tracking-code.php';
+	}
+	?>
+</head>
 
-						$duplicate_social_icons = ob_get_contents();
+<body <?php body_class(); ?> <?php uds_wp_body_attributes(); ?>>
 
-						ob_end_clean();
+	<?php
+	do_action( 'wp_body_open' );
 
-						printf(
-							'<div class="et_duplicate_social_icons">
-								%1$s
-							</div>',
-							$duplicate_social_icons
-						);
-					}
+	// Site Google Tag Manager (noscript).
+	if ( ! empty( $site_gtm_container_id ) ) {
+		include get_stylesheet_directory() . '/inc/analytics/google-tag-manager-noscript-code.php';
+	}
+	?>
 
-					if ( '' !== $et_secondary_nav ) {
-						echo $et_secondary_nav;
-					}
-
-					et_show_cart_total();
-				?>
-				</div> <!-- #et-secondary-menu -->
-
-			</div> <!-- .container -->
-		</div> <!-- #top-header -->
-	<?php endif; // true ==== $et_top_info_defined ?>
-
-	<?php if ( $et_slide_header || is_customize_preview() ) : ?>
-		<div class="et_slide_in_menu_container">
-			<?php if ( 'fullscreen' === et_get_option( 'header_style', 'left' ) || is_customize_preview() ) { ?>
-				<span class="mobile_menu_bar et_toggle_fullscreen_menu"></span>
-			<?php } ?>
-
-			<?php
-				if ( $et_contact_info_defined || true === $show_header_social_icons || false !== et_get_option( 'show_search_icon', true ) || class_exists( 'woocommerce' ) || is_customize_preview() ) { ?>
-					<div class="et_slide_menu_top">
-
-					<?php if ( 'fullscreen' === et_get_option( 'header_style', 'left' ) ) { ?>
-						<div class="et_pb_top_menu_inner">
-					<?php } ?>
-			<?php }
-
-				if ( true === $show_header_social_icons ) {
-					get_template_part( 'includes/social_icons', 'header' );
-				}
-
-				et_show_cart_total();
-			?>
-			<?php if ( false !== et_get_option( 'show_search_icon', true ) || is_customize_preview() ) : ?>
-				<?php if ( 'fullscreen' !== et_get_option( 'header_style', 'left' ) ) { ?>
-					<div class="clear"></div>
-				<?php } ?>
-				<form role="search" method="get" class="et-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<?php
-						printf( '<input type="search" class="et-search-field" placeholder="%1$s" value="%2$s" name="s" title="%3$s" />',
-							esc_attr__( 'Search &hellip;', 'Divi' ),
-							get_search_query(),
-							esc_attr__( 'Search for:', 'Divi' )
-						);
-					?>
-					<button type="submit" id="searchsubmit_header"></button>
-				</form>
-			<?php endif; // true === et_get_option( 'show_search_icon', false ) ?>
-
-			<?php if ( $et_contact_info_defined ) : ?>
-
-				<div id="et-info">
-				<?php if ( '' !== ( $et_phone_number = et_get_option( 'phone_number' ) ) ) : ?>
-					<span id="et-info-phone"><?php echo et_sanitize_html_input_text( $et_phone_number ); ?></span>
-				<?php endif; ?>
-
-				<?php if ( '' !== ( $et_email = et_get_option( 'header_email' ) ) ) : ?>
-					<a href="<?php echo esc_attr( 'mailto:' . $et_email ); ?>"><span id="et-info-email"><?php echo esc_html( $et_email ); ?></span></a>
-				<?php endif; ?>
-				</div> <!-- #et-info -->
-
-			<?php endif; // true === $et_contact_info_defined ?>
-			<?php if ( $et_contact_info_defined || true === $show_header_social_icons || false !== et_get_option( 'show_search_icon', true ) || class_exists( 'woocommerce' ) || is_customize_preview() ) { ?>
-				<?php if ( 'fullscreen' === et_get_option( 'header_style', 'left' ) ) { ?>
-					</div> <!-- .et_pb_top_menu_inner -->
-				<?php } ?>
-
-				</div> <!-- .et_slide_menu_top -->
-			<?php } ?>
-
-			<div class="et_pb_fullscreen_nav_container">
-				<?php
-					$slide_nav = '';
-					$slide_menu_class = 'et_mobile_menu';
-
-					$slide_nav = wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'echo' => false, 'items_wrap' => '%3$s' ) );
-					$slide_nav .= wp_nav_menu( array( 'theme_location' => 'secondary-menu', 'container' => '', 'fallback_cb' => '', 'echo' => false, 'items_wrap' => '%3$s' ) );
-				?>
-
-				<ul id="mobile_menu_slide" class="<?php echo esc_attr( $slide_menu_class ); ?>">
-
-				<?php
-					if ( '' == $slide_nav ) :
-				?>
-						<?php if ( 'on' == et_get_option( 'divi_home_link' ) ) { ?>
-							<li <?php if ( is_home() ) echo( 'class="current_page_item"' ); ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Home', 'Divi' ); ?></a></li>
-						<?php }; ?>
-
-						<?php show_page_menu( $slide_menu_class, false, false ); ?>
-						<?php show_categories_menu( $slide_menu_class, false ); ?>
-				<?php
-					else :
-						echo( $slide_nav );
-					endif;
-				?>
-
-				</ul>
+	<header id="asu-header" class="fixed-top">
+		<div id="wrapper-header-top">
+			<div class="container-xl">
+				<div class="row">
+					<div id="header-top" class="col-12">
+						<nav class="nav" aria-label="Top">
+							<a class="nav-link sr-only sr-only-focusable" href="#skip-to-content">Skip to Content</a>
+							<a class="nav-link sr-only sr-only-focusable" href="http://asu.edu/accessibility/feedback?a11yref=unity-design-system">Report an accessibility problem</a>
+							<a class="nav-link" href="https://asu.edu">ASU Home</a>
+							<a class="nav-link" href="https://my.asu.edu">My ASU</a>
+							<a class="nav-link" href="https://asu.edu/about/colleges-and-schools">Colleges and Schools</a>
+							<div class="nav-link login-status">
+								<a class="signin" href="https://my.asu.edu">Sign In</a>
+							</div>
+							<form class="form-inline" action="https://search.asu.edu/search" method="get" name="gs">
+								<input class="form-control" type="search" name="q" aria-labelledby="header-top-search" required>
+								<label id="header-top-search">Search ASU</label>
+								<input name="site" value="default_collection" type="hidden">
+								<input name="sort" value="date:D:L:d1" type="hidden">
+								<input name="output" value="xml_no_dtd" type="hidden">
+								<input name="ie" value="UTF-8" type="hidden">
+								<input name="oe" value="UTF-8" type="hidden">
+								<input name="client" value="asu_frontend" type="hidden">
+								<input name="proxystylesheet" value="asu_frontend" type="hidden">
+							</form>
+						</nav>
+					</div>
+				</div>
 			</div>
 		</div>
-	<?php endif; // true ==== $et_slide_header ?>
 
-		<header id="main-header" data-height-onload="<?php echo esc_attr( et_get_option( 'menu_height', '66' ) ); ?>">
-			<div class="container clearfix et_menu_container">
-			<?php
-				$logo = ( $user_logo = et_get_option( 'divi_logo' ) ) && '' != $user_logo
-					? $user_logo
-					: $template_directory_uri . '/images/logo.png';
-			?>
-				<div class="logo_container">
-					<span class="logo_helper"></span>
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-						<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo" data-height-percentage="<?php echo esc_attr( et_get_option( 'logo_height', '54' ) ); ?>" />
-					</a>
-				</div>
-				<div id="et-top-navigation" data-height="<?php echo esc_attr( et_get_option( 'menu_height', '66' ) ); ?>" data-fixed-height="<?php echo esc_attr( et_get_option( 'minimized_menu_height', '40' ) ); ?>">
-					<?php if ( ! $et_slide_header || is_customize_preview() ) : ?>
-						<nav id="top-menu-nav">
-						<?php
-							$menuClass = 'nav';
-							if ( 'on' == et_get_option( 'divi_disable_toptier' ) ) $menuClass .= ' et_disable_top_tier';
-							$primaryNav = '';
+		<div id="wrapper-header-main">
+			<div class="container-xl">
+				<div class="row">
+					<div id="header-main" class="col-12">
+						<nav class="navbar navbar-expand-xl" aria-label="Main">
 
-							$primaryNav = wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'menu_id' => 'top-menu', 'echo' => false ) );
+							<a class="navbar-brand" href="https://www.asu.edu">
+								<img class="vert" src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/asu-unity/asu-logo/asu-university-vert-maroongold.png" alt="Arizona State University" />
+								<img class="horiz" src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/asu-unity/asu-logo/asu-university-horiz-maroongold.png" alt="Arizona State University" />
+							</a>
 
-							if ( '' == $primaryNav ) :
-						?>
-							<ul id="top-menu" class="<?php echo esc_attr( $menuClass ); ?>">
-								<?php if ( 'on' == et_get_option( 'divi_home_link' ) ) { ?>
-									<li <?php if ( is_home() ) echo( 'class="current_page_item"' ); ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Home', 'Divi' ); ?></a></li>
-								<?php }; ?>
+							<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#menubar" aria-controls="menubar" aria-expanded="false" aria-label="Toggle navigation">
+								<span title="Open mobile menu" class="fa fa-bars"></span>
+								<span title="Close mobile menu" class="fa-stack">
+									<i class="fa fa-circle fa-stack-2x"></i>
+									<i class="fa fa-times fa-stack-1x"></i>
+								</span>
+							</button>
 
-								<?php show_page_menu( $menuClass, false, false ); ?>
-								<?php show_categories_menu( $menuClass, false ); ?>
-							</ul>
-						<?php
-							else :
-								echo( $primaryNav );
-							endif;
-						?>
+							<?php
+							// <div class="navbar-container"> +/- a "no-link" class.
+							uds_wp_render_navbar_container();
+							?>
+
+								<?php
+								// <div class="title"> +/- a utility class.
+								uds_wp_render_title_wrapper();
+								?>
+
+									<?php uds_wp_render_parent_unit_name(); ?>
+									<?php uds_wp_render_subdomain_name(); ?>
+
+								</div><!-- .end .title -->
+
+								<div class="collapse navbar-collapse w-100 justify-content-between" id="menubar">
+
+									<?php uds_wp_render_main_nav_menu(); ?>
+
+									<div class="navbar-mobile-footer">
+										<form class="form-inline navbar-mobile-search" action="https://search.asu.edu/search" method="get" name="gs">
+											<input class="form-control" type="search" name="q" aria-label="Search" placeholder="Search ASU">
+											<input name="site" value="default_collection" type="hidden">
+											<input name="sort" value="date:D:L:d1" type="hidden">
+											<input name="output" value="xml_no_dtd" type="hidden">
+											<input name="ie" value="UTF-8" type="hidden">
+											<input name="oe" value="UTF-8" type="hidden">
+											<input name="client" value="asu_frontend" type="hidden">
+											<input name="proxystylesheet" value="asu_frontend" type="hidden">
+										</form>
+										<div class="nav-grid">
+											<a class="nav-link" href="https://asu.edu">ASU Home</a>
+											<a class="nav-link" href="https://my.asu.edu">My ASU</a>
+											<a class="nav-link" href="https://asu.edu/about/colleges-and-schools">Colleges and Schools</a>
+											<div class="nav-link login-status">
+												Sparky<a class="signout" href="https://webapp4.asu.edu/myasu/Signout">Sign Out</a>
+											</div>
+										</div>
+									</div>
+
+								</div>
+							</div>
+
 						</nav>
-					<?php endif; ?>
-
-					<?php
-					if ( ! $et_top_info_defined && ( ! $et_slide_header || is_customize_preview() ) ) {
-						et_show_cart_total( array(
-							'no_text' => true,
-						) );
-					}
-					?>
-
-					<?php if ( $et_slide_header || is_customize_preview() ) : ?>
-						<span class="mobile_menu_bar et_pb_header_toggle et_toggle_<?php echo esc_attr( et_get_option( 'header_style', 'left' ) ); ?>_menu"></span>
-					<?php endif; ?>
-
-					<?php if ( ( false !== et_get_option( 'show_search_icon', true ) && ! $et_slide_header ) || is_customize_preview() ) : ?>
-					<div id="et_top_search">
-						<span id="et_search_icon"></span>
 					</div>
-					<?php endif; // true === et_get_option( 'show_search_icon', false ) ?>
-
-					<?php do_action( 'et_header_top' ); ?>
-				</div> <!-- #et-top-navigation -->
-			</div> <!-- .container -->
-			<div class="et_search_outer">
-				<div class="container et_search_form_container">
-					<form role="search" method="get" class="et-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<?php
-						printf( '<input type="search" class="et-search-field" placeholder="%1$s" value="%2$s" name="s" title="%3$s" />',
-							esc_attr__( 'Search &hellip;', 'Divi' ),
-							get_search_query(),
-							esc_attr__( 'Search for:', 'Divi' )
-						);
-					?>
-					</form>
-					<span class="et_close_search_field"></span>
 				</div>
 			</div>
-		</header> <!-- #main-header -->
+		</div>
 
-		<div id="et-main-area">
+	</header>
+
+	<?php do_action( 'uds_wp_after_global_header' ); ?>
